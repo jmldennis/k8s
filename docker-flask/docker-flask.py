c04775418d22fbbf2690fff352ac8e9cc6304dc8
jmldennis <jmldennis@gmail.com>
2022-06-02 19:55:25 -0500

    simple app finished... i think

--- docker-flask/docker-flask.py
+++ docker-flask/docker-flask.py
@@ -1,14 +1,23 @@
 from flask import Flask, request
+import socket
 
 #Initialize Flask App
 app = Flask(__name__)
 port = 5005
 
+#Get Hostname
+host_name = socket.gethostname()
+version = "1.0"
+
+hits = 0
+
 @app.route('/', methods=['GET'])
 def index():
+    global hits
     """Receive a notification from Webex Teams and handle it"""
     if request.method == 'GET':
-        return f'Request received on local port {port}'
+        hits+=1
+        return f'{host_name} version {version} has received {hits} requests on local port {port}'
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=port)
